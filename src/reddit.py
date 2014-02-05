37439daa4866990a48ae3b8fff220a3e567f124a
Jake Marsh <jakemmarsh@gmail.com>
2014-02-05 16:36:34 -0500

    added logic to keep track of posts that have already been retrieved/posted, to prevent duplicates

--- src/reddit.py
+++ src/reddit.py
@@ -7,21 +7,38 @@ class API(object):
     # initialize reddit client
     def __init__(self):
         self.r = praw.Reddit(user_agent=config.get('reddit', 'userAgent'))
+        self.alreadyProcessed = []
         
     # get new posts from a subreddit
     def getNewPosts(self, subreddit, limit, after = None):
+        returnPosts = []
         if(after):
             posts = self.r.get_subreddit(subreddit).get_new(limit=limit, params={'after': after})
         else:
             posts = self.r.get_subreddit(subreddit).get_new(limit=limit)
         
-        return [post for post in posts]
+        # make sure we're only returning posts that haven't already been retrieved before
+        for post in posts:
+            if(post.id not in self.alreadyProcessed):
+                returnPosts.append(post)
+                # mark as processed
+                self.alreadyProcessed.append(post.id)
+                
+        return returnPosts
     
     # get hot posts from a subreddit
     def getHotPosts(self, subreddit, limit, after = None):
+        returnPosts = []
         if(after):
             posts = self.r.get_subreddit(subreddit).get_hot(limit=limit, params={'after': after})
         else:
             posts = self.r.get_subreddit(subreddit).get_hot(limit=limit)
         
-        return [post for post in posts]
+        # make sure we're only returning posts that haven't already been retrieved before
+        for post in posts:
+            if(post.id not in self.alreadyProcessed):
+                returnPosts.append(post)
+                # mark as processed
+                self.alreadyProcessed.append(post.id)
+                
+        return returnPosts
