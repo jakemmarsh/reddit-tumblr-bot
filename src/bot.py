1ff0068e987a75da2a558f885e6c19bfc345a7b0
Jake Marsh <jakemmarsh@gmail.com>
2014-10-20 13:44:52 -0400

    Complete all todos by implementing regex logic

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,4 @@
-import tumblr, reddit, time
+import tumblr, reddit, time, re
 
 class Bot():
     def __init__(self, subreddit, queryType = 'hot', limit = 20, timer = 3600):
@@ -10,10 +10,11 @@ class Bot():
         self.queryType = queryType
         self.limit = limit
         self.latest = None
-        
+
     # get latest posts from specified subreddit via reddit API
     def getLatestRedditPosts(self):
         functionName = 'get%sPosts' % self.queryType.lower().title()
+
         # check to see if query should be paginated
         if(self.latest is not None):
             # use getattr in order to use variable as function call name
@@ -21,118 +22,93 @@ class Bot():
         else:
             # use getattr in order to use variable as function call name
             posts = getattr(self.redditAPI, functionName)(subreddit=self.subreddit, limit = self.limit)
-        
+
         # update self.latest for later paginated queries
         if(len(posts) > 0):
             self.latest = "t3_" + str(posts[-1].id)
-        
+
         return posts
-        
+
     # get song artist from reddit post title
     def getSongArtist(self, postTitle):
-        # TODO: use regex
-        for i in range(0, len(postTitle)):
-            if(postTitle[i] == '-'):
-                # return song artist and remaining section of title
-                # TODO: use real unescaping
-                return postTitle[0:i-1], postTitle[i+2:].replace('&amp;', '&')
-        
-    # get song title from remaining unprocessed portion of reddit post title
-    def getSongTitle(self, remainingPostTitle):
-        # TODO: use regex
-        for i in range(0, len(remainingPostTitle)):
-            if(remainingPostTitle[i] == '['):
-                # return song title and remaining section of title
-                return remainingPostTitle[0:i-1], remainingPostTitle[i:]
-            
-    # get song genres from remaining unprocessed portion of reddit post title
-    def getSongGenres(self, remainingPostTitle):
-        # TODO: use regex
-        allGenres = ''
-        for i in range(0, len(remainingPostTitle)):
-            if(remainingPostTitle[i] == '['):
-                while(remainingPostTitle[i+1] != ']'):
-                    allGenres += str(remainingPostTitle[i+1])
-                    i += 1
-                else:
-                    if '/' in allGenres:
-                        genres = [x.strip(' ').title() for x in allGenres.split('/')]
-                    elif ',' in allGenres:
-                        genres = [x.strip(' ').title() for x in allGenres.split(',')]
-                    else:
-                        genres = [allGenres.strip(' ').title()]
-                    
-                    return genres, remainingPostTitle[i:]
-        
-    # get song year from remaining unprocessed portion of reddit post title
-    def getSongYear(self, remainingPostTitle):
-    # TODO: use regex
-        year = ''
-        for i in range(0, len(remainingPostTitle)):
-            if(remainingPostTitle[i] == '('):
-                while(remainingPostTitle[i+1] != ')' and remainingPostTitle[i+1].isdigit()):
-                    year += str(remainingPostTitle[i+1])
-                    i += 1
-                else:
-                    # only return year if a valid one was found
-                    if(len(year) > 0):
-                        return year
-                    else:
-                        return None
-    
+        try:
+            return re.search('- (.+)', postTitle).groups()[0].replace('&amp;', '&')
+        except:
+            return None
+
+    # get song title from reddit post title
+    def getSongTitle(self, postTitle):
+        try:
+            return re.search('(.+) -', postTitle).groups()[0]
+        except:
+            return None
+
+    # get song genres from reddit post title
+    def getSongGenres(self, postTitle):
+        try:
+            allGenres = re.search('\[(.+)\]', postTitle).groups()[0];
+        except:
+            return None
+
+        if '/' in allGenres:
+            genres = [x.strip(' ').title() for x in allGenres.split('/')]
+        elif ',' in allGenres:
+            genres = [x.strip(' ').title() for x in allGenres.split(',')]
+        else:
+            genres = [allGenres.strip(' ').title()]
+
+        return genres
+
+    # get song year from reddit post title
+    def getSongYear(self, postTitle):
+        try:
+            return re.search('(\d+)', postTitle).groups()[0];
+        except:
+            return None
+
     # pull necessary information from reddit posts
     def getFormattedRedditPosts(self):
         redditPosts = self.getLatestRedditPosts()
-        
         formattedPosts = []
-        
+
         for post in redditPosts:
             formattedPost = {}
-            
+
             # only parse and save post if it isn't a self post
-            # TODO: use regex?
-            if('www.reddit.com' not in post.url.lower()):
-                # get URL
+            if(not re.search('!reddit.com', post.url.lower(), re.IGNORECASE)):
                 formattedPost['url'] = post.url
-                # get artist
-                formattedPost['artist'], remainingTitle = self.getSongArtist(post.title)
-                # get song title
-                formattedPost['songTitle'], remainingTitle = self.getSongTitle(remainingTitle)
-                # get genres
-                formattedPost['genres'], remainingTitle = self.getSongGenres(remainingTitle)
-                # get year
-                formattedPost['songYear'] = self.getSongYear(remainingTitle)
-                    
+                formattedPost['artist'] = self.getSongArtist(post.title)
+                formattedPost['songTitle'] = self.getSongTitle(post.title)
+                formattedPost['genres'] = self.getSongGenres(post.title)
+                formattedPost['songYear'] = self.getSongYear(post.title)
+
                 # only process songs newer than 2012
                 if(formattedPost['songYear'] is not None and int(formattedPost['songYear']) > 2012):
                     formattedPosts.append(formattedPost)
-        
+
         return formattedPosts
-    
+
     # create Tumblr posts for all retrieved reddit posts
-    def createTumblrPosts(self, redditPosts):  
+    def createTumblrPosts(self, redditPosts):
         for post in redditPosts:
             # do something with youtube links
-            # TODO: use regex?
-            if('youtube.com' in post['url'].lower()):
+            if(re.search('youtube.com', post['url'], re.IGNORECASE)):
                 self.tumblrAPI.createYoutubePost(post)
-                
+
             # do something with vimeo links
-            # TODO: use regex?
-            if('vimeo.com' in post['url'].lower()):
+            if(re.search('vimeo.com', post['url'], re.IGNORECASE)):
                 self.tumblrAPI.createVimeoPost(post)
-            
+
             # do something with audio links
-            # TODO: use regex?
-            elif('soundcloud.com' in post['url'].lower() or 'bandcamp.com' in post['url'].lower()):
+            elif(re.search('soundcloud.com|bandcamp.com', post['url'], re.IGNORECASE)):
                 self.tumblrAPI.createAudioPost(post)
-        
+
     # query for reddit posts and subsequently create Tumblr posts
     def process(self):
         redditPosts = self.getFormattedRedditPosts()
         if(len(redditPosts) > 0):
             self.createTumblrPosts(redditPosts)
-    
+
     def run(self):
         cycleCount = 0
         while True:
@@ -142,6 +118,6 @@ class Bot():
                     self.latest = None
                     cycleCount = 0
                 cycleCount += 1
-                    
+
             self.process()
             time.sleep(self.timer)
